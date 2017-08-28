ef9fe419ec59de9df07d08bd76c060f8fc9b6f0e
Jiho Choi <choi296@purdue.edu>
2017-08-27 22:53:00 -0400

    file io

--- scala/ScalaFileIO.scala
+++ scala/ScalaFileIO.scala
@@ -3,19 +3,23 @@
 
 
 import scala.io._
-import scala.collection.mutable.ArrayBuffer
 
+import java.io.PrintWriter
 
 object ScalaTuple {
 	def main(args: Array[String]) {
 
-		var tupleMarge = (103, "Marge Simpson", 10.25)
+		val writer = new PrintWriter("test.txt")
+		writer.write("sample text\nsome more random text")
+		writer.close()
 
-		printf("%s owes us $%.2f\n", tupleMarge._2, tupleMarge._3)
+		val textFromFile = Source.fromFile("test.txt", "UTF-8")
 
-		tupleMarge.productIterator.foreach {i => println(i)}
+		val lineIterator = textFromFile.getLines
 
-		println(tupleMarge.toString())
+		for( line <- lineIterator) {
+			println(line)
+		}
 	}
 
 }
